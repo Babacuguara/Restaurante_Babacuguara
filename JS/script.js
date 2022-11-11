19fdcd3d5c31faa199e0dec258fafcb98c5d9c6f
Babacuguara <babacuguara@gmail.com>
2022-11-10 21:50:58 -0300

    alteração html

--- JS/script.js
+++ JS/script.js
@@ -23,11 +23,11 @@ document.getElementById("radio1").checked = true;
 
 setInterval( function(){
     nextImage();
-},   2000)
+},   2000);
 
 function nextImage(){
     count++;
-    if(count>5){
+    if(count>4){
         count = 1;
     }
     document.getElementById("radio"+count).checked = true;
